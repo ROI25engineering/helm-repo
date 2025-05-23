6585c66e7aa3ba311c8efb7b9eef9a295b991bbf
antonio.marchi <antonio.marchi@thinkalpha.io>
2025-05-23 06:58:34 +0200

    fix: massive replacement of 168.0 with 168.68

--- simple-service/publish-helm-chart.sh
+++ simple-service/publish-helm-chart.sh
@@ -13,10 +13,10 @@ aws s3 cp "$PACKAGE_NAME" s3://lespro-charts/ --endpoint-url https://lon1.digita
 
 helm package . --version 0.1.4
 mc cp ./simple-service-0.1.4.tgz myminio/roi25-engineering-helm-simple-chart/
-helm repo index . --url http://192.168.0.44:9000/roi25-engineering-helm-simple-chart
+helm repo index . --url http://192.168.68.44:9000/roi25-engineering-helm-simple-chart
 mc cp ./index.yaml myminio/roi25-engineering-helm-simple-chart/
 
 # Test:
 helm template . --name-template downloader-ws --namespace downloader-ws-test \
-  --set image.repository=192.168.0.45:5000/downloader-ws \
+  --set image.repository=192.168.68.45:5000/downloader-ws \
   --set image.tag=0.32.0
