1fbcac187bb4f9b54184195c8721f2e721c3fe72
hamzabenayed <74600689+hamzabenayed@users.noreply.github.com>
2026-02-24 15:18:22 +0100

    update: updated simple-service chart to include helm params securing api

--- simple-service/publish-helm-chart.sh
+++ simple-service/publish-helm-chart.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Extract the version from Chart.yaml
-CHART_VERSION=$(grep '^version:' Chart.yaml | awk '{print $2}')
-CHART_NAME=$(grep '^name:' Chart.yaml | awk '{print $2}')
+
+CHART_VERSION=$(awk '/^version:/{print $2}' Chart.yaml)
+CHART_NAME=$(awk '/^name:/{print $2}' Chart.yaml)
 PACKAGE_NAME="${CHART_NAME}-${CHART_VERSION}.tgz"
 
+REPO_URL="http://192.168.68.45:9000/helm-chart"
+MINIO_PATH="/helm-chart"
+
 helm package .
-helm repo index . --url https://lespro-charts.lon1.digitaloceanspaces.com/
-aws s3 cp index.yaml s3://lespro-charts/ --endpoint-url https://lon1.digitaloceanspaces.com --acl public-read
-aws s3 cp "$PACKAGE_NAME" s3://lespro-charts/ --endpoint-url https://lon1.digitaloceanspaces.com --acl public-read
 
+curl -fsS "${REPO_URL}/index.yaml" -o index.remote.yaml
+helm repo index . --url "${REPO_URL}" --merge index.remote.yaml
 
-helm package . --version 0.1.4
-mc cp ./simple-service-0.1.4.tgz myminio/roi25-engineering-helm-simple-chart/
-helm repo index . --url http://192.168.68.44:9000/roi25-engineering-helm-simple-chart
-mc cp ./index.yaml myminio/roi25-engineering-helm-simple-chart/
+mc cp "./${PACKAGE_NAME}" "${MINIO_PATH}/"
+mc cp ./index.yaml "${MINIO_PATH}/"
 
-# Test:
-helm template . --name-template downloader-ws --namespace downloader-ws-test \
-  --set image.repository=192.168.68.45:5000/downloader-ws \
-  --set image.tag=0.32.0
+curl -fsS "${REPO_URL}/index.yaml" | grep "${PACKAGE_NAME}"
+curl -fsS "${REPO_URL}/index.yaml" | grep "${PACKAGE_NAME}"
